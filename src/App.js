435b9b36b73de6eba1c5020bf2b3e8f363209ab8
imadhithya <adhithyabalaji.ca@gmail.com>
2024-02-06 10:59:25 -0500

    Add my name to App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Class!
+          Hello Adhithya!
         </p>
         <a
           className="App-link"
